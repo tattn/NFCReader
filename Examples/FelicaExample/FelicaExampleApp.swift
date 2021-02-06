37b0735351d7b002450cdfb9eb1a105251ad7d31
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2021-02-06 19:58:55 +0900

    Restructure an example

---
+++ Examples/FelicaExample/FelicaExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FelicaExampleApp.swift
+//  FelicaExample
+//
+//  Created by Tatsuya Tanaka on 2021/02/06.
+//
+
+import SwiftUI
+
+@main
+struct FelicaExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
